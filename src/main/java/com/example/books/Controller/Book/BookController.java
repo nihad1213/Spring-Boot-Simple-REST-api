e941457048bf401cb190fcafb3b2103ddb4b5d34
nihad1213 <nihad.nemetli@gmail.com>
2025-10-15 01:52:44 +0400

    new: BookCOntroller created

---
+++ src/main/java/com/example/books/Controller/Book/BookController.java
@@ -0,0 +1,11 @@
+package com.example.books.Controller.Book;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("api/books")
+public class BookController {
+    // Enpoints will be writed in here
+}
