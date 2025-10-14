e0be4a90f35d9dc011ed9f0cf93a58f5bdc7d345
nihad1213 <nihad.nemetli@gmail.com>
2025-10-15 01:45:32 +0400

    new: HealthController created, application properties added for database connection, pom.xml updated for postgresql

---
+++ src/main/java/com/example/books/Controller/Health/HealthController.java
@@ -0,0 +1,28 @@
+package com.example.books.Controller.Health;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RestController
+public class HealthController {
+    private final DataSource dataSource;
+
+    public HealthController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        try (Connection connection = dataSource.getConnection()) {
+            if (connection.isValid(2)) {
+                return "Connection is OK!";
+            }
+        } catch (Exception e) {
+            return "Connection is FAILED!";
+        }
+
+        return "UNKNOWN!";
+    }
+}
