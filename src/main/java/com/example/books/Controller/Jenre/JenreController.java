6b085e474d36c32a0dde365c06b4771074878e90
nihad1213 <nihad.nemetli@gmail.com>
2025-10-15 01:56:45 +0400

    JenreCOntroller created

---
+++ src/main/java/com/example/books/Controller/Jenre/JenreController.java
@@ -0,0 +1,11 @@
+package com.example.books.Controller.Jenre;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/jenre/")
+public class JenreController {
+    // Enpoints will be writed in here
+}
