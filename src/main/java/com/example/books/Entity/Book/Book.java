fcc4456009d3c64638c89b1a463c5ca12965613f
nihad1213 <nihad.nemetli@gmail.com>
2025-10-17 21:12:17 +0400

    new: Book Entity created

--- src/main/java/com/example/books/Entity/Book/Book.java
+++ src/main/java/com/example/books/Entity/Book/Book.java
@@ -1,4 +1,35 @@
 package com.example.books.Entity.Book;
 
+import com.example.books.Entity.Author.Author;
+import com.example.books.Entity.Genre.Genre;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    @ManyToMany
+    @JoinTable(
+            name = "book_genre",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id")
+    )
+    private List<Genre> genres;
+
 }
