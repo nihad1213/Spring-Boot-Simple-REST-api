9a3de5bab995a26ea1cc861c744e3e91a97d74b5
nihad1213 <nihad.nemetli@gmail.com>
2025-10-17 21:05:36 +0400

    new: Genre Entity created

--- src/main/java/com/example/books/Entity/Genre/Genre.java
+++ src/main/java/com/example/books/Entity/Genre/Genre.java
@@ -1,4 +1,20 @@
 package com.example.books.Entity.Genre;
 
+import com.example.books.Entity.Book.Book;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToMany(mappedBy = "genres")
+    private List<Book> books;
 }
