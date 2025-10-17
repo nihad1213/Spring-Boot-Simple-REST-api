cbefcac9ad994a28031175f2fc335f195132341b
nihad1213 <nihad.nemetli@gmail.com>
2025-10-17 20:52:43 +0400

    fix: "User changed to Author"

--- src/main/java/com/example/books/Controller/Author/AuthorController.java
+++ src/main/java/com/example/books/Controller/Author/AuthorController.java
@@ -1,13 +1,12 @@
-package com.example.books.Controller.User;
+package com.example.books.Controller.Author;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/user/")
-public class UserController {
+public class AuthorController {
     // Endpoints will be writed in here
 }
