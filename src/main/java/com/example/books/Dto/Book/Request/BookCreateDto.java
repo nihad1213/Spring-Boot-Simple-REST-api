4c5748a05f80cd6fef2ce7b4e0354c78be10a3da
nihad1213 <nihad.nemetli@gmail.com>
2025-10-18 19:35:56 +0400

    new: Books Dtos created

---
+++ src/main/java/com/example/books/Dto/Book/Request/BookCreateDto.java
@@ -0,0 +1,16 @@
+package com.example.books.Dto.Book.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookCreateDto {
+    private String title;
+    private Long authorId;
+    private List<Long> genreIds;
+}
---
+++ src/main/java/com/example/books/Dto/Book/Response/BookShowDto.java
@@ -0,0 +1,17 @@
+package com.example.books.Dto.Book.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookShowDto {
+    private Long id;
+    private String title;
+    private String authorName;
+    private List<String> genres;
+}
